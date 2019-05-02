8ef0e8863606ed3106073f38555f5da3c6df5aee
Andrew Ang <marc.kunanit@gmail.com>
2019-05-02 12:33:26 -0400

    feat: add serial field to vehicle

--- rentals/models.py
+++ rentals/models.py
@@ -2,12 +2,15 @@ from django.db import models
 
 
 class User(models.Model):
-    username = models.CharField(max_length=200, unique=True)
-
+    username = models.CharField(max_length=200)
 
 class Vehicle(models.Model):
-    type = models.CharField(max_length=200, choices=[('scooter','scooter'),('bike','bike')])
-
+    type = models.CharField(
+        max_length=200,
+        choices=[('scooter','scooter'),('bike','bike')]
+    )
+    serial = models.CharField(max_length=100)
+    
     def last_trip_start(self):
         return self.trip_set.order_by('-start_time').first() if self.trip_set.exists() else None
         
@@ -28,4 +31,3 @@ class Trip(models.Model):
     end_x = models.FloatField(blank=True)
     end_y = models.FloatField(blank=True)
     end_time = models.DateTimeField(blank=True)
-
